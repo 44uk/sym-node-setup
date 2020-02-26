5a248fda8374f7f8467eaf60f72a0c4513a7096f
Yoshiyuki Ieyama <yukku0423@gmail.com>
2020-02-26 22:41:09 +0900

    renamed

--- symbol-node-setup.sh
+++ symbol-node-setup.sh
@@ -71,7 +71,7 @@ apt-get update -y && apt-get upgrade -y
 # dockerのインストール
 
 ## Ubuntu ---------------------------------------------------- {
-apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common git
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update -y && apt-get install -y docker-ce
